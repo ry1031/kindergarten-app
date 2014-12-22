6d4525dcc4b4915779f0c9281fbb3f5d2bc4d3be
ry1031 <rayy1031@gmail.com>
2014-12-23 01:30:10 +0800

    Add project skeleton

---
+++ src/edu/cuhk/cmsc/kindergarten/ParentAreaActivity.java
@@ -0,0 +1,13 @@
+package edu.cuhk.cmsc.kindergarten;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ParentAreaActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_parent_area);
+	}
+}
