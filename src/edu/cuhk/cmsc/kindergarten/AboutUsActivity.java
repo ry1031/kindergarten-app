55811a8087cbc2936c7f8a259a8299e76fcef00e
ry1031 <rayy1031@gmail.com>
2014-12-28 02:03:50 +0800

    Add about us activity and contact activity

--- src/edu/cuhk/cmsc/kindergarten/AboutUsActivity.java
+++ src/edu/cuhk/cmsc/kindergarten/AboutUsActivity.java
@@ -2,6 +2,9 @@ package edu.cuhk.cmsc.kindergarten;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class AboutUsActivity extends Activity {
 
@@ -9,5 +12,9 @@ public class AboutUsActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about_us);
+		
+		TextView textView = (TextView) findViewById(R.id.about_school);
+		textView.setText(Html.fromHtml(getString(R.string.about_school)));
+		textView.setMovementMethod(new ScrollingMovementMethod());
 	}
 }
--- src/edu/cuhk/cmsc/kindergarten/ContactActivity.java
+++ src/edu/cuhk/cmsc/kindergarten/ContactActivity.java
@@ -2,6 +2,12 @@ package edu.cuhk.cmsc.kindergarten;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.method.ScrollingMovementMethod;
+import android.text.util.Linkify;
+import android.widget.TextView;
 
 public class ContactActivity extends Activity {
 
@@ -9,5 +15,16 @@ public class ContactActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_contact);
+		
+		TextView textView = (TextView) findViewById(R.id.contact);
+		textView.setMovementMethod(new ScrollingMovementMethod());
+		textView.setMovementMethod(LinkMovementMethod.getInstance());
+		
+		String message = getString(R.string.contact_us);
+		final SpannableString mes = new SpannableString(Html.fromHtml(message));
+		Linkify.addLinks(mes, Linkify.EMAIL_ADDRESSES | Linkify.PHONE_NUMBERS);
+		
+
+		textView.setText(mes);
 	}
 }
