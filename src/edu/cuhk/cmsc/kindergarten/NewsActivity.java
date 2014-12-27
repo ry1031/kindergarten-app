0221c6f120b99bba7a4ad8a09266a8bd3153eb6a
ry1031 <rayy1031@gmail.com>
2014-12-28 03:25:59 +0800

    Add parent area activity and news activity

--- src/edu/cuhk/cmsc/kindergarten/NewsActivity.java
+++ src/edu/cuhk/cmsc/kindergarten/NewsActivity.java
@@ -2,6 +2,9 @@ package edu.cuhk.cmsc.kindergarten;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class NewsActivity extends Activity {
 
@@ -9,5 +12,9 @@ public class NewsActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_news);
+		
+		TextView textView = (TextView) findViewById(R.id.news);
+		textView.setText(Html.fromHtml(getString(R.string.news_content)));
+		textView.setMovementMethod(new ScrollingMovementMethod());
 	}
 }
--- src/edu/cuhk/cmsc/kindergarten/ParentAreaActivity.java
+++ src/edu/cuhk/cmsc/kindergarten/ParentAreaActivity.java
@@ -2,6 +2,12 @@ package edu.cuhk.cmsc.kindergarten;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.method.ScrollingMovementMethod;
+import android.text.util.Linkify;
+import android.widget.TextView;
 
 public class ParentAreaActivity extends Activity {
 
@@ -9,5 +15,16 @@ public class ParentAreaActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_parent_area);
+		
+		TextView textView = (TextView) findViewById(R.id.cooking_course_info);
+		textView.setMovementMethod(new ScrollingMovementMethod());
+		textView.setMovementMethod(LinkMovementMethod.getInstance());
+		
+		String message = getString(R.string.cooking_course_info);
+		final SpannableString mes = new SpannableString(Html.fromHtml(message));
+		Linkify.addLinks(mes, Linkify.EMAIL_ADDRESSES);
+		
+
+		textView.setText(mes);
 	}
 }
